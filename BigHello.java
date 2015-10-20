5db1e68222674c578308113965f1843e94e2ebfa
vadimm81 <knitelw@yahoo.de>
2015-10-20 16:05:22 +0200

    Aufgabe 1

---
+++ BigHello.java
@@ -0,0 +1,10 @@
+class BigHello {
+    public static void main(String[] args) {
+		System.out.println(" _   _      _ _         __        __         _     _ _ ");
+		System.out.println("| | | | ___| | | ___    \\ \\      / /__  _ __| | __| | |");
+		System.out.println("| |_| |/ _ \\ | |/ _ \\    \\ \\ /\\ / / _ \\| '__| |/ _` | |");
+		System.out.println("|  _  |  __/ | | (_) |    \\ V  V / (_) | |  | | (_| |_|");
+		System.out.println("|_| |_|\\___|_|_|\\___( )    \\_/\\_/ \\___/|_|  |_|\\__,_(_)");
+		System.out.println("                    |/");        
+    }
+}
